dc08210113d9055be0eaf3df892d35c24be88af9
Shamix <nsammi11@gmail.com>
2018-11-10 19:58:24 -0500

    Task: PageController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    function home(){
+        return view('pages.home');
+    }
+    function home(){
+        return view('pages.about');
+    }
+    function home(){
+        return view('pages.contact');
+    }
+    function home(){
+        return view('pages.thankyou');
+    }
 }
+
+
